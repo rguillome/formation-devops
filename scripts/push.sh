f0cc82ba6570ef568d72feb1379078a794ac4387
Alice Bob <alice@eni.fr>
2018-09-20 17:11:51 +0200

    correction of push command

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_78a1bf3a-c60b-4246-9248-b41b74f74f2c.git
-git --verbose --force push cleverapps master
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push-par-clevercloud-customers.services.clever-cloud.com/app_fc888331-7799-44f8-a0bf-6dd0af90c84b.git
+git push --force --verbose cleverapps master 
